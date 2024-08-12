1541d6b5c6c858e33c5f96f1a8be970260b998d8
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2024-08-12 16:44:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Vertical Tree Traversal/vertical-tree-traversal.java
@@ -0,0 +1,154 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                Solution obj = new Solution();
+                ArrayList <Integer> res = obj.verticalOrder(root);
+                for (Integer num : res) System.out.print(num + " "); 
+                System.out.println();
+    	        
+	        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    // Function to find the vertical order traversal of Binary Tree.
+    static class info {
+        Node root;
+        int hd;
+        public info(Node root, int hd) {
+            this.root = root;
+            this.hd = hd;
+        }
+    }
+
+    static ArrayList<Integer> verticalOrder(Node root) {
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        Queue<info> q = new LinkedList<>();
+        q.add(new info(root, 0));
+        while (!q.isEmpty()) {
+            info curr = q.poll();
+            map.computeIfAbsent(curr.hd, k -> new ArrayList<>()).add(curr.root.data);
+
+            min = Math.min(min, curr.hd);
+            max = Math.max(max, curr.hd);
+            if (curr.root.left != null) {
+                q.add(new info(curr.root.left, curr.hd - 1));
+            }
+            if (curr.root.right != null) {
+                q.add(new info(curr.root.right, curr.hd + 1));
+            }
+        }
+        for (int i = min; i <= max; i++) {
+            result.addAll(map.get(i));
+        }
+
+        return result;
+    }
+}
