68d01b9ba9d5ce926ce8cc106e8008c528cff25d
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2025-04-28 17:36:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String inputLine[] = read.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int sum = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.count(arr, sum));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int count(int coins[], int sum) {
+        int dp[][]=new int[sum+1][coins.length];
+        for(int d[]:dp){
+            Arrays.fill(d,-1);
+        }
+        return solve(coins,sum,0,dp); 
+    }
+    public int solve(int coins[],int sum,int last,int dp[][]){
+        if(sum==0){
+            return 1;
+        }
+        if(sum<0){
+            return 0;
+        }
+        if(dp[sum][last]!=-1){
+            return dp[sum][last];
+        }
+        int count=0;
+        for(int i=last;i<coins.length;i++){
+            count+=solve(coins,sum-coins[i],i,dp);
+        }
+        return dp[sum][last]=count;
+    }
+}
